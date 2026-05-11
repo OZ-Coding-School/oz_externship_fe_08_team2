f4fc6b459461ce11d407abec1fc4ccb18d06f049
최민제 <alswpchl0818@gmail.com>
2026-05-11 19:06:40 +0900

    fix: AuthInitializer → useInitAuth로 교체, 인증 초기화 안정화
    - TanStack Query 기반 AuthInitializer 제거 (서비스 워커 타이밍 의존 문제)
    - 직접 axios 호출하는 useInitAuth 사용으로 성공/실패 모두 확실히 처리
    - useInitAuth URL trailing slash 수정 (/me → /me/)

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import { useEffect } from 'react'
 import { RouterProvider } from '@/providers/RouterProvider'
-import { useAuthStore } from '@/stores/authStore'
-import { useCurrentUser } from '@/features/accounts/me'
-
-function AuthInitializer() {
-  const login = useAuthStore((state) => state.login)
-  const setInitialized = useAuthStore((state) => state.setInitialized)
-  const { data, isError } = useCurrentUser()
-
-  useEffect(() => {
-    if (data) {
-      login({
-        id: data.id,
-        nickname: data.nickname,
-        email: data.email,
-        profileImage: data.profile_img_url ?? null,
-      })
-    } else if (isError) {
-      setInitialized()
-    }
-  }, [data, login, isError, setInitialized])
-
-  return null
-}
+import { useInitAuth } from '@/hooks/useInitAuth'
 
 function App() {
-  return (
-    <>
-      <AuthInitializer />
-      <RouterProvider />
-    </>
-  )
+  useInitAuth()
+  return <RouterProvider />
 }
 
 export default App
